45df0b657c1ca79c25c0c0a3b5dbb25eda75bf9e
kangkung17 <mmuhdar17@gmail.com>
2020-09-20 03:23:02 +0700

    convert HTML to svelte components

---
+++ src/data/charities.js
@@ -0,0 +1,18 @@
+module.exports = {
+  charities: [
+    {
+      id: 1,
+      title: 'First Charity Project',
+      category: 'Money',
+      thumbnail:
+        'https://www.investopedia.com/thmb/KLIT6B4uZN8EekDpI-lij18iyjQ=/1500x844/smart/filters:no_upscale()/GettyImages-1054017850-7ef42af7b8044d7a86cfb2bff8641e1d.jpg',
+      pledged: 99000,
+      target: 100000,
+      date_end: +new Date('30 December 2020'),
+      profile_photo:
+        'https://lh3.googleusercontent.com/proxy/WQ3gk0pDbq6MoS-lOa9jC4U3tM91Gex1YuW_v0DIWE9gMyafqNZjt6MF54WE_lwrixke5Z8U51pCeYyoyNU5pUZLCa9ECBDjjy4e',
+      profile_name: 'Anna Liebert',
+      no_pledges: 0,
+    },
+  ],
+};
